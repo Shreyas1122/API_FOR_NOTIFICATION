8367780ed482d43f707b91705ff329751d056c08
Shreyas1122 <110128821+Shreyas1122@users.noreply.github.com>
2025-07-11 00:36:16 +0530

    Update firstserver.js

--- firstserver.js
+++ firstserver.js
@@ -32,6 +32,7 @@ const store=new mongodbsession({
 //firebase notification code is here 
  
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
